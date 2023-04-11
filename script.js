849cc59326169792baa2fb9289a73f3fb973c4fb
Maya Ganeva <mayagganeva@gmail.com>
2023-04-11 20:41:23 +0300

    fixed hysteresis bug :)

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-const observer = new IntersectionObserver((entries) => {
-	entries.forEach((entry) => {
+const observer = new IntersectionObserver(entries => {
+	entries.forEach(entry => {
 		console.log(entry)
 		if (entry.isIntersecting) {
 			entry.target.classList.add('show');
-		} else {
-			entry.target.classList.remove('show');
-		}
+		} 
 	});
 });
 
